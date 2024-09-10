25c0f407a6e4a4a3e830500b0a1ab4af0ca49952
Olívia Helena <olivia.helena071@gmail.com>
2024-09-10 19:22:11 -0300

    Update

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ import plotly.express as px
 
 @st.cache_resource
 def load_model():
-    #https://drive.google.com/file/d/1YwyK9Ps8p04S3uPm60zShwMVYmu6sce8/view?usp=drive_link
-    url = 'https://drive.google.com/uc?id=1YwyK9Ps8p04S3uPm60zShwMVYmu6sce8'
-
+    url = 'https://drive.google.com/uc?id=1S3-lb3WG2pIsfRS93Qyx_cPOXJwbRnnE'
     gdown.download(url, 'quant_model16bits.tflite')
     interpreter = tf.lite.Interpreter(model_path='quant_model16bits.tflite')
     interpreter.allocate_tensors()
@@ -33,14 +31,11 @@ def load_image():
 
         return image
 
-
-def prevision(interpreter,image):
-
+def prevision(interpreter, image):
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
     
-    interpreter.set_tensor(input_details[0]['index'],image) 
-    
+    interpreter.set_tensor(input_details[0]['index'], image) 
     interpreter.invoke()
     
     output_data = interpreter.get_tensor(output_details[0]['index'])
@@ -48,14 +43,12 @@ def prevision(interpreter,image):
 
     df = pd.DataFrame()
     df['classes'] = classes
-    df['probabilities (%)'] = 100*output_data[0]
+    df['probabilities (%)'] = 100 * output_data[0]
 
     fig = px.bar(df, y='classes', x='probabilities (%)', orientation='h', text='probabilities (%)',
                  title='Probability of your skin type')
     
     st.plotly_chart(fig)
-
-
 
 def main():
     st.set_page_config(
@@ -67,11 +60,11 @@ def main():
     interpreter = load_model()
 
     # Load image
-    image = load_image
+    image = load_image()
 
-    # Classify
     if image is not None:
-        prevision(interpreter,image)
+        prevision(interpreter, image)
 
 if __name__ == "__main__":
     main()
+
